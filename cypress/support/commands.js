98a4c2cc8b0040dda0f9d5bf171ec3860bdea5de
Paola Rosales <prosales@banksa.com.ar>
2022-12-14 22:50:27 -0300

    creacion nuevos archivos

---
+++ cypress/support/commands.js
@@ -0,0 +1,34 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('ReactElement', (element, $tag) => {
+	// Command para buscar elemento de React por primer parent y child.
+	if ($tag !== undefined) {
+		cy.get(`[data-react-toolbox='${element}']` + ` ${$tag}`)
+	} else {
+		cy.get(`[data-react-toolbox='${element}']`)
+	}
+})
